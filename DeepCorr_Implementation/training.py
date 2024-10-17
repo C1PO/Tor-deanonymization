e27432c3a424316e3b32ded7b874f32e0c08ad0b
Ludovico Latini <latiniludovico@gmail.com>
2024-10-17 10:07:08 +0200

    rebase on main

--- DeepCorr_Implementation/training.py
+++ DeepCorr_Implementation/training.py
@@ -5,20 +5,23 @@ import tensorflow as tf
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
-def train_model(l2s, labels):
+def train_model():
+    l2s, labels, l2s_test, labels_test = generate_data()
+
     input_shape = (8, 500,1) 
     dropout_keep_prob = 0.5
     batch_size = 256
-    epochs = 2
+    epochs = 200
 
     model = model_cnn(input_shape, dropout_keep_prob)
 
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
-                loss='binary_crossentropy',
+                loss='binary_crossentropy',  # Adatto per classificazione binaria
                 metrics=['accuracy'])
 
     model.summary()
 
+    '''
     history = model.fit(l2s, labels, 
                         batch_size=batch_size, 
                         epochs=epochs)
@@ -27,3 +30,6 @@ def train_model(l2s, labels):
     if acc > 0.8:
         model.save('model.h5')
         print("Modello salvato!")
+    '''
+    print("faccio il train")
+    model.save('model.h5')
