fdaed4776c489ca86ad6eeabd717dcfe427d5fc7
Ludovico Latini <latiniludovico@gmail.com>
2025-01-07 18:21:09 +0100

    Desktop correlation

---
+++ Ip_leacker/mod_socks.sh
@@ -0,0 +1,14 @@
+SOCKS_PORT=1024
+while true; do
+    if ss -tulnp | grep -q ":$SOCKS_PORT"; then
+    if curl --socks5 127.0.0.1:$SOCKS_PORT https://icanhazip.com; then
+        echo "Tor è stato avviato sulla porta $SOCKS_PORT."
+        curl --socks5 127.0.0.1:$SOCKS_PORT https://icanhazip.com
+        curl --socks5 127.0.0.1:$SOCKS_PORT https://icanhazip.com
+        curl https://icanhazip.com
+        break
+        fi
+    fi
+    echo "Tor non è nella $SOCKS_PORT"
+    SOCKS_PORT=$((SOCKS_PORT + 1))
+done
---
+++ Ip_leacker/random_script.py
@@ -0,0 +1,16 @@
+from setproctitle import setproctitle
+import random
+import string
+import subprocess
+import time
+
+
+def randomize_process_name():
+    name = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+    setproctitle(name)
+def start_tor():
+    while True:
+        randomize_process_name()
+        subprocess.run(["tor"])
+        time.sleep(10)
+start_tor()
